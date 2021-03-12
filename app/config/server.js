06b974129f07e9b9c713c6ec01e173630451419a
wbuc <wbuchling@live.com>
2021-03-12 20:29:04 +0000

    Init

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '188dcf58788b33d869b82b5ebe118984'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6572ec23-2138-4a4f-a6a7-d6053d4d75a0'
+};
